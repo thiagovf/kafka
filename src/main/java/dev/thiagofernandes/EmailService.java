81c3f7e6bb071a29c528b4282e05d3e1a614d6fa
Thiago <4077420+thiagovf@users.noreply.github.com>
2022-01-03 11:20:16 -0300

    Email service

--- src/main/java/dev/thiagofernandes/EmailService.java
+++ src/main/java/dev/thiagofernandes/EmailService.java
@@ -8,17 +8,17 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-public class FraudDetectorService {
+public class EmailService {
 	public static void main(String[] args) {
 		var consumer = new KafkaConsumer<>(properties());
-		consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+		consumer.subscribe(Collections.singletonList("ECOMMERCE_SEND_EMAIL"));
 		while (true) {
 			var records = consumer.poll(Duration.ofMillis(100));
 			if (!records.isEmpty()) {
 				System.out.println("Encontrei " + records.count() + " registros.");
 				for (var record : records) {
 					System.out.println("---------------------");
-					System.out.println("Processing new order, checking for fraud");
+					System.out.println("Sending email");
 					System.out.println(record.key());
 					System.out.println(record.value());
 					System.out.println(record.partition());
@@ -29,7 +29,7 @@ public class FraudDetectorService {
 						// ignoring
 						e.printStackTrace();
 					}
-					System.out.println("Order Processed.");
+					System.out.println("Email sent.");
 				}
 			}
 		}
@@ -40,7 +40,7 @@ public class FraudDetectorService {
 		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, EmailService.class.getSimpleName());
 
 		return properties;
 	}
